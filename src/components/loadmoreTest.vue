20a0ee25b1b2ea050a7369b64888ecd323dbe343
wanghaibo <wang.haibo@chinaott.net>
2018-03-17 19:31:22 +0800

    test

--- src/components/loadmoreTest.vue
+++ src/components/loadmoreTest.vue
@@ -1,49 +1,78 @@
 <template>
-  <div>
-    <div class="page-infinite-wrapper" ref="wrapper">
-    <mt-loadmore :bottom-method="loadBottom" @bottom-status-change="handleBottomChange" :bottom-all-loaded="allLoaded"  ref="loadmore">
-      <ul>
-        <li v-for="item in list">{{item}}</li>
-      </ul>
-      <div slot="bottom" class="mint-loadmore-bottom">
-        <span v-show="bottomStatus !== 'loading'" :class="{ 'is-rotate': bottomStatus === 'drop' }">↑上拉加载更多</span>
-        <span v-show="bottomStatus === 'loading'">
-                  <mt-spinner type="snake"></mt-spinner>
-                </span>
-      </div>
-      <p class="loading-bottom" v-if="allLoaded">没有更多数据了.</p>
-    </mt-loadmore>
+  <div class="page-loadmore">
+    <div class="page-loadmore-wrapper" ref="wrapper" :style="{ height: wrapperHeight + 'px' }">
+      <mt-loadmore :bottom-method="loadBottom" @bottom-status-change="handleBottomChange" :bottom-all-loaded="allLoaded" ref="loadmore">
+        <ul class="page-loadmore-list">
+          <li v-for="item in list" class="page-loadmore-listitem">{{ item }}</li>
+        </ul>
+        <div slot="bottom" class="mint-loadmore-bottom">
+          <span v-if="allLoaded== false" v-show="bottomStatus !== 'loading'" :class="{ 'is-rotate': bottomStatus === 'drop' }">↑加载更多</span>
+          <span v-show="bottomStatus === 'loading'">
+            <mt-spinner type="snake"></mt-spinner>
+          </span>
+          <span v-if="allLoaded">没有更多数据了!</span>
+        </div>
+      </mt-loadmore>
     </div>
   </div>
 </template>
-<script>
+
+<script type="text/babel">
   export default {
     data() {
       return {
-        allLoaded : false,
-        list: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
+        list: [],
+        allLoaded: false,
         bottomStatus: '',
-      }
+        wrapperHeight: 0
+      };
     },
+
     methods: {
-      loadBottom: function(){
-        let last = this.list[this.list.length - 1];
-        for (let i = 1; i <= 10; i++) {
-          this.list.push(last + i);
-        }
-        if(this.list.length >= 100){
-          this.allLoaded = true;// 若数据已全部获取完毕
-        }
-        this.$refs.loadmore.onBottomLoaded();
-      },
       handleBottomChange(status) {
         this.bottomStatus = status;
       },
+      handleInit: function(){
+        setTimeout(() => {
+          for (let i = 1; i <= 20; i++) {
+            this.list.push(i);
+          }
+          this.allLoaded = false;
+        }, 3000);
+      },
+      handleMore: function(){
+        let lastValue = this.list[this.list.length - 1];
+        setTimeout(() => {
+          for (let i = 1; i <= 10; i++) {
+            this.list.push(lastValue + i);
+          }
+        },3000);
+      },
+      loadBottom() {
+        let lastValue = this.list[this.list.length - 1];
+        console.log(lastValue);
+        if (lastValue < 40) {
+          this.handleMore();
+        } else {
+          this.allLoaded = true;
+        }
+          this.$refs.loadmore.onBottomLoaded();
+
+      }
+    },
+
+    created() {
+      this.handleInit();
+    },
+
+    mounted() {
+      this.wrapperHeight = document.documentElement.clientHeight - this.$refs.wrapper.getBoundingClientRect().top;
     }
-  }
+  };
 </script>
+
 <style scoped>
-  li{
-    height: 2rem;
-  }
+.page-loadmore-listitem{
+  height: 100px;
+}
 </style>
